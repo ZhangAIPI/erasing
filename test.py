a69272611c70541c5bd037eda42b5c50a805a0cf
zeliang <zzh@ur.rochester.edu>
2025-03-18 08:33:58 -0400

    update

--- test.py
+++ test.py
@@ -78,6 +78,7 @@ for prompt in prompts:
 
     # torch load
     masked_prompt_embedding = torch.load(new_pl_path)
+    import pdb;pdb.set_trace()
     images = diffuser(prompt,
              img_size=512,
              n_steps=50,
