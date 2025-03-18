7b4910c12ccad76e94df4613b09032b4cb866c58
zeliang <zzh@ur.rochester.edu>
2025-03-18 07:52:16 -0400

    update

--- pl_diffusers.py
+++ pl_diffusers.py
@@ -103,7 +103,7 @@ def train(erase_concept, erase_from, train_method, iterations, negative_guidance
             loss = criteria(negative_latents, target_latents - (negative_guidance*(positive_latents - neutral_latents))) 
 
             loss.backward(retain_graph=True)
-            import pdb;pdb.set_trace()
+            # import pdb;pdb.set_trace()
             # optimizer.step()
             to_optimize_embeddings = to_optimize_embeddings - lr * torch.sign(to_optimize_embeddings.grad)
             
@@ -128,7 +128,7 @@ if __name__ == '__main__':
     parser.add_argument('--erase_from', help='target concept to erase from', type=str, required=False, default = None)
     parser.add_argument('--train_method', help='Type of method (xattn, noxattn, full, xattn-strict', type=str, required=True)
     parser.add_argument('--iterations', help='Number of iterations', type=int, default=200)
-    parser.add_argument('--lr', help='Learning rate', type=float, default=2e-5)
+    parser.add_argument('--lr', help='Learning rate', type=float, default=1e-3)
     parser.add_argument('--negative_guidance', help='Negative guidance value', type=float, required=False, default=1)
     parser.add_argument('--save_path', help='Path to save model', type=str, default='models/')
     parser.add_argument('--device', help='cuda device to train on', type=str, required=False, default='cuda:0')
