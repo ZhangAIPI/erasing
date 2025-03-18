1cee3aaf837d5fd4788363fba2be41352ce8b0e0
zeliang <zzh@ur.rochester.edu>
2025-03-18 07:37:50 -0400

    update

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 esd_path = 'models/esd-vangogh_from_vangogh-xattn_1-epochs_200.pt'
 pl_path = 'models/pl-vangogh_from_vangogh-xattn_1-epochs_200.pt'
+new_pl_path = 'models/new_pl-vangogh_from_vangogh-xattn_1-epochs_200.pt'
 train_method = 'xattn' ## REMEMBER: please use the same train_method you used for training (it is present in the saved name)
 
 diffuser = StableDiffuser(scheduler='DDIM').to('cuda')
@@ -21,42 +22,62 @@ import random
 seed = random.randint(0,2**15)
 seed = 41
 print(seed)
-prompts = ["[MASK]"] # ["Van Gogh", "unicorn draw with the Van Gogh style", "unicorn"]
+prompts = ["Van Gogh", "unicorn draw with the Van Gogh style", "unicorn"]
 for prompt in prompts:
-    images = diffuser(prompt,
-             img_size=512,
-             n_steps=50,
-             n_imgs=1,
-             generator=torch.Generator().manual_seed(seed),
-             guidance_scale=7.5
-             )[0][0]
-    images = transforms.ToTensor()(images)
-    plt.imshow(images.permute(1,2,0).cpu().numpy())
-    plt.axis('off')
-    plt.savefig('figs/ori_{}.png'.format(prompt.replace(' ','_')))
-
-
-
-    with finetuner:
-        images = diffuser(prompt,
-                 img_size=512,
-                 n_steps=50,
-                 n_imgs=1,
-                 generator=torch.Generator().manual_seed(seed),
-                 guidance_scale=7.5
-                 )[0][0]
-    images = transforms.ToTensor()(images)
-    plt.imshow(images.permute(1,2,0).cpu().numpy())
-    plt.axis('off')
-    plt.savefig('figs/est_{}.png'.format(prompt.replace(' ','_')))
-
-
+    # images = diffuser(prompt,
+    #          img_size=512,
+    #          n_steps=50,
+    #          n_imgs=1,
+    #          generator=torch.Generator().manual_seed(seed),
+    #          guidance_scale=7.5
+    #          )[0][0]
+    # images = transforms.ToTensor()(images)
+    # plt.imshow(images.permute(1,2,0).cpu().numpy())
+    # plt.axis('off')
+    # plt.savefig('figs/ori_{}.png'.format(prompt.replace(' ','_')))
+# 
+# 
+# 
+    # with finetuner:
+    #     images = diffuser(prompt,
+    #              img_size=512,
+    #              n_steps=50,
+    #              n_imgs=1,
+    #              generator=torch.Generator().manual_seed(seed),
+    #              guidance_scale=7.5
+    #              )[0][0]
+    # images = transforms.ToTensor()(images)
+    # plt.imshow(images.permute(1,2,0).cpu().numpy())
+    # plt.axis('off')
+    # plt.savefig('figs/est_{}.png'.format(prompt.replace(' ','_')))
+# 
+# 
+    # n_opt_prompts = 3
+    # masked_prompt_embedding = None
+# 
+    # # torch load
+    # masked_prompt_embedding = torch.load(pl_path)
+# 
+    # images = diffuser(prompt,
+    #          img_size=512,
+    #          n_steps=50,
+    #          n_imgs=1,
+    #          generator=torch.Generator().manual_seed(seed),
+    #          guidance_scale=7.5,
+    #          n_opt_prompts=n_opt_prompts,
+    #         masked_prompt_embedding=masked_prompt_embedding
+    #          )[0][0]
+    # images = transforms.ToTensor()(images)
+    # plt.imshow(images.permute(1,2,0).cpu().numpy())
+    # plt.axis('off')
+    # plt.savefig('figs/pl_{}.png'.format(prompt.replace(' ','_')))
+    
+    
     n_opt_prompts = 3
     masked_prompt_embedding = None
 
     # torch load
-    masked_prompt_embedding = torch.load(pl_path)
-
+    masked_prompt_embedding = torch.load(new_pl_path)
     images = diffuser(prompt,
              img_size=512,
              n_steps=50,
@@ -69,4 +90,4 @@ for prompt in prompts:
     images = transforms.ToTensor()(images)
     plt.imshow(images.permute(1,2,0).cpu().numpy())
     plt.axis('off')
-    plt.savefig('figs/pl_{}.png'.format(prompt.replace(' ','_')))
+    plt.savefig('figs/new_pl_{}.png'.format(prompt.replace(' ','_')))
